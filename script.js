7c3a854452ca342e361e3c661cbfca4a75b6a7c2
partapu <preethampartapu029@gmail.com>
2021-08-23 15:48:08 +0530

    Updated local storage

--- script.js
+++ script.js
@@ -22,29 +22,32 @@ form.addEventListener("submit", function (e) {
   item.innerHTML = str;
   todolists.insertAdjacentElement("beforeend", item);
   input.value = "";
-  arr.push(str);
-  localStorage.setItem("todo", JSON.stringify(arr));
-  console.log(arr);
+  UpdateLocalStorage();
 });
 function addToDo(str) {
   const item = document.createElement("li");
   item.classList.add("todoitem");
   item.innerHTML = str;
   todolists.insertAdjacentElement("beforeend", item);
+  if (item.querySelector("h4").classList.contains("strick")) {
+    item.querySelector(".closebtn").addEventListener("click", function (e) {
+      e.target.closest(".todoitem").remove();
+      UpdateLocalStorage();
+    });
+  }
 }
 todolists.addEventListener("click", function (e) {
   const todoitem = e.target.closest(".todoitem");
   if (!todoitem) return;
-  if (todoitem.classList.contains("strick")) {
-    todoitem.remove();
+  if (todoitem.querySelector("h4").classList.contains("strick")) {
+    return;
   }
   todoitem.querySelector("h4").classList.add("strick");
   todoitem.insertAdjacentHTML("afterbegin", `<span class="closebtn">X</span>`);
+  UpdateLocalStorage();
   todoitem.querySelector(".closebtn").addEventListener("click", function (e) {
     e.target.closest(".todoitem").remove();
-    arr.splice(arr.indexOf(todoitem.innerHTML), 1);
-    localStorage.setItem("todo", JSON.stringify(arr));
-    console.log(arr);
+    UpdateLocalStorage();
   });
 });
 
@@ -54,3 +57,10 @@ const timer = function () {
 };
 timer();
 setInterval(timer, 1000);
+
+function UpdateLocalStorage() {
+  let notesEl = document.querySelectorAll("li");
+  notesEl = Array.from(notesEl);
+  notesEl = notesEl.map((el) => el.innerHTML);
+  localStorage.setItem("todo", JSON.stringify(notesEl));
+}
